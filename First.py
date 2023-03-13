478f1c0a38c3abaa2d1efa3317c0dbfef6bbda87
Irenfeliz <irenfeliz@gmail.com>
2023-03-13 17:59:19 +0300

    commit

--- First.py
+++ First.py
@@ -1,7 +1,14 @@
 n = int(input())
 l = []
-for i in range(n):
-    k = int(input())
-    l.append(k)
-del l[1::2]
+l2 = []
+for _ in range(n):
+    s = input()
+    l.append(s)
+k = int(input())
 print(l)
+for i in range(len(l)):
+    l2.extend(l[i])
+    if k <= len(l2):
+        print(l2[k-1], end ='')
+    del l2[:]
+
