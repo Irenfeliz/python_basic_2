a98953ea15ebbc1ea74b0df60a9ec2581c2710cc
Irenfeliz <irenfeliz@gmail.com>
2023-03-14 13:20:19 +0300

    commit

--- First.py
+++ First.py
@@ -1,7 +1,43 @@
-n = int(input())
-l = []
-for i in range(n):
-    k = int(input())
-    l.append(k)
-del l[1::2]
-print(l)
+# s = input()
+# print(*s.split(), sep='\n')
+
+# s = input()
+# l = s.split()
+# for i in range(len(l)):
+#     print(l[i][0] + '.', end='')
+
+# s = input()
+# l = s.split('\\')
+# print(*l, sep='\n')
+
+# s = input()
+# l = s.split()
+# for i in l:
+#     print('+' * int(i))
+
+# s = input()
+# l = s.split('.')
+# for i in range(len(l)):
+#     l[i] = int(l[i])
+#     if 0 <= l[i] <= 255:
+#         flag = True
+#     else:
+#         flag = False
+#         break
+# if flag:
+#     print('ДА')
+# else:
+#     print('НЕТ')
+
+# s = input()
+# r = input()
+# print(r.join(s))
+
+s = input()
+l = s.split()
+count = 0
+for i in range(len(l)):
+    for j in range(i+1, len(l)):
+        if l[i] == l[j]:
+            count += 1
+print(count)
