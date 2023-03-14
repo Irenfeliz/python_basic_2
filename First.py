aa8c8c814b930352cfbad166a216f0bcb04317d8
Irenfeliz <irenfeliz@gmail.com>
2023-03-14 13:43:42 +0300

    commit

--- First.py
+++ First.py
@@ -1,7 +1,19 @@
-n = int(input())
-l = []
-for i in range(n):
-    k = int(input())
-    l.append(k)
-del l[1::2]
-print(l)
+numbers = [8, 9, 10, 11]
+
+numbers.pop(1)
+numbers.insert(1, 17)
+for i in range(4, 7):
+    numbers.append(i)
+numbers.pop(0)
+numbers += numbers
+numbers.insert(3, 25)
+print(numbers)
+
+# еще одно решение
+# numbers = [8, 9, 10, 11]
+# numbers[1] = 17
+# numbers.extend([4, 5, 6])
+# del numbers[0]
+# numbers *= 2
+# numbers.insert(3, 25)
+# print(numbers)
